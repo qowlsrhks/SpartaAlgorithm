274689c1125636241aa3642a3a984415c870169d
qowlsrhks <skswlsrhks@naver.com>
2024-07-31 12:45:27 +0900

    첫번째 문제

--- src/main/java/org/example/day1/두수의차.java
+++ src/main/java/org/example/day1/두수의차.java
@@ -1,4 +1,22 @@
 package org.example.day1;
 
 public class 두수의차 {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+        int answer = 0;
+        answer = new 두수의차().solution(num1, num2);
+        System.out.println("answer = " + answer);
+
+        System.out.println("answer = " + new 두수의차().solution(num1 = 100, num2 = 2));
+    }
+
+    int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 - num2;
+
+        return answer;
+    }
 }
